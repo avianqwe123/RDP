8e22395c8836d78d0df8f0341378182e98d25780
Subibhu <59786652+Subibhu@users.noreply.github.com>
2020-12-29 12:01:30 +0545

    Update and rename RDP.sh to rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m Mpnetization
-sudo adduser Mpnetization sudo
-echo 'Mpnetization:8426' | sudo chpasswd
+read -p "Enter Username For RDP: " berysed
+sudo useradd -m $berysed
+sudo adduser $berysed sudo
+echo '$berysed:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +19,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser Mpnetization chrome-remote-desktop
+sudo adduser $berysed chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - Mpnetization -c """$CRP"""
+su - $berysed -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
